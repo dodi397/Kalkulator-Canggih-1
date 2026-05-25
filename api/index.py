5f1906dbe1a95c08f5f460fe1989883156f307d0
dodi397 <dodi397dodi@example.com>
2026-05-25 15:39:14 +0700

    Memperbaiki

--- api/index.py
+++ api/index.py
@@ -1,27 +1,17 @@
-import os
-import sys
-import traceback
-
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, BASE_DIR)
-
 from flask import Flask
+
+from calculator_app.routes_main import main_bp
+from calculator_app.routes_arithmetic import arithmetic_bp
+from calculator_app.routes_logic import logic_bp
+from calculator_app.routes_transform import transform_bp
 
 app = Flask(__name__)
 
+app.register_blueprint(main_bp)
+app.register_blueprint(arithmetic_bp)
+app.register_blueprint(logic_bp)
+app.register_blueprint(transform_bp)
+
 @app.route("/")
 def home():
-    try:
-        from api.calculator_app.routes_main import main_bp
-        from api.calculator_app.routes_arithmetic import arithmetic_bp
-        from api.calculator_app.routes_logic import logic_bp
-        from api.calculator_app.routes_transform import transform_bp
-
-        app.register_blueprint(main_bp)
-        app.register_blueprint(arithmetic_bp)
-        app.register_blueprint(logic_bp)
-        app.register_blueprint(transform_bp)
-
-        return "Berhasil!"
-    except Exception:
-        return "<pre>" + traceback.format_exc() + "</pre>", 500
+    return "Deploy berhasil"
