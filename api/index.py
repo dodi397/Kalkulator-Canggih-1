1b68b3b3a77597857ef02cf06a3430a4378264ac
dodi397 <dodi397dodi@example.com>
2026-05-25 14:47:10 +0700

    Memperbaiki

--- api/index.py
+++ api/index.py
@@ -1,23 +1,28 @@
+import os
+import sys
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, BASE_DIR)
+
 from flask import Flask
 from calculator_app.routes_main import main_bp
 from calculator_app.routes_arithmetic import arithmetic_bp
 from calculator_app.routes_logic import logic_bp
 from calculator_app.routes_transform import transform_bp
-from calculator_app.history import init_session_history
 
 app = Flask(
     __name__,
     template_folder="../templates",
     static_folder="../static"
-    )
+)
 
-app.secret_key = "smartcalc-pro-secret-key-change-me"
+app.secret_key = "secret"
 
 app.register_blueprint(main_bp)
-app.register_blueprint(arithmetic_bp, url_prefix="/aritmatika")
-app.register_blueprint(logic_bp, url_prefix="/logika")
-app.register_blueprint(transform_bp, url_prefix="/transformasi")
+app.register_blueprint(arithmetic_bp)
+app.register_blueprint(logic_bp)
+app.register_blueprint(transform_bp)
 
-@app.before_request
-def ensure_history():
-    init_session_history()
+@app.route("/")
+def home():
+    return "Website berhasil deploy!"
