d528f4cf06cf983e89b93722bb4d5ccdc98d0606
dodi397 <dodi397dodi@example.com>
2026-05-25 15:15:41 +0700

    Memindahkan folder calculator

--- api/index.py
+++ api/index.py
@@ -1,27 +1,25 @@
-import os
-import sys
-import traceback
-
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, BASE_DIR)
-
 from flask import Flask
+from calculator_app.routes_main import main_bp
+from calculator_app.routes_arithmetic import arithmetic_bp
+from calculator_app.routes_logic import logic_bp
+from calculator_app.routes_transform import transform_bp
+from calculator_app.history import init_session_history
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
+
+app.secret_key = "secret"
+
+init_session_history(app)
+
+app.register_blueprint(main_bp)
+app.register_blueprint(arithmetic_bp)
+app.register_blueprint(logic_bp)
+app.register_blueprint(transform_bp)
 
 @app.route("/")
 def home():
-    try:
-        from calculator_app.routes_main import main_bp
-        from calculator_app.routes_arithmetic import arithmetic_bp
-        from calculator_app.routes_logic import logic_bp
-        from calculator_app.routes_transform import transform_bp
-
-        app.register_blueprint(main_bp)
-        app.register_blueprint(arithmetic_bp)
-        app.register_blueprint(logic_bp)
-        app.register_blueprint(transform_bp)
-
-        return "Berhasil!"
-    except Exception:
-        return "<pre>" + traceback.format_exc() + "</pre>", 500
+    return "Website berhasil deploy!"
